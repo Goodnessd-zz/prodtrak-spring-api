39dfbd56d525c973d406c96e3c0e52006fa3ce65
Goodness Daramola <gdaramol@thoughtworks.com>
2018-07-20 14:56:40 -0500

    [gd] enabling cors without basic authentication

---
+++ src/main/java/demo/api/authenticate/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package demo.api.authenticate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        // TODO: research whether and how to enable this
+        http.csrf().disable();
+    }
+
+    @Bean
+    @Lazy
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
